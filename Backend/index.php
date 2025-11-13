17f711714020150e7c6efe9beb7c1fdabab837dc
AljaKratovac <alja.kratovac@stu.ibu.edu.ba>
2025-11-13 15:49:39 +0100

    Updated composer

--- Backend/index.php
+++ Backend/index.php
@@ -1,6 +1,7 @@
 <?php
 require 'vendor/autoload.php';
 
+
 require_once __DIR__ . '/rest/services/UserService.php';
 require_once __DIR__ . '/rest/services/ProductService.php';
 require_once __DIR__ . '/rest/services/PaymentService.php';
